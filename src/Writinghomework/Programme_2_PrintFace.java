aa84f4c4de690e47440d05e343c78dc6e953cba0
Urveel <urveelp@gmail.com>
2023-10-18 23:28:32 +0100

    https://github.com/Urveel/java-week1--writing-hw-urveel-patel.git

---
+++ src/Writinghomework/Programme_2_PrintFace.java
@@ -0,0 +1,14 @@
+package Writinghomework;
+
+public class Programme_2_PrintFace {
+    /**
+     * 2.Write a java program to print a face.
+     */
+    public static void main(String[] args) {
+        System.out.println("+\"\"\"\"\"+");
+        System.out.println("[|  0 0   |]");
+        System.out.println(" |   ^    | ");
+        System.out.println(" |  ' _'  | ");
+        System.out.println(" +________| ");
+    }
+}
---
+++ src/Writinghomework/Programme_8_DivisionOfTwoNumber.java
@@ -0,0 +1,17 @@
+package Writinghomework;
+/**
+ * 8. Write a Java program to divide two numbers and print on the screen.
+ * Test Data: 50/3
+ * Expected Output: 16
+ */
+public class Programme_8_DivisionOfTwoNumber {
+    public static void main(String[] args) {
+        //The Two integers are value1 and value2
+        int value1 =50;
+        int value2=3;
+        int division = value1 / value2;
+        //Concatenation
+        System.out.println("The" +value1+"is divided by"+value2+"the answer is 2+division");
+
+    }
+}
